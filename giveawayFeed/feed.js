8cd4640773a98813f3a7ed79b5326d2e1cefe36a
Ron Lapushner <lapushnerron@gmail.com>
2017-12-05 19:45:52 -0600

    removed test line

--- giveawayFeed/feed.js
+++ giveawayFeed/feed.js
@@ -5,11 +5,4 @@ const
 
 const feed = tweeterFeed.pipe(tweetToGiveaway)
 
-feed.on('readable', () => {
-  console.log('readable')
-  console.log(feed.read().toString())
-})
-
-setTimeout(() => { console.log('read'); console.log(feed.read().toString()) } , 60000 * 5)
-
 module.exports = feed

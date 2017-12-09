ed31590259ada3eb68b0eeb610929cbbce62ba6c
Ron Lapushner <lapushnerron@gmail.com>
2017-12-08 21:54:00 -0600

    amazon session acquisition

--- giveawayClicker/getAmazonSession.js
+++ giveawayClicker/getAmazonSession.js
@@ -1,8 +1,10 @@
 const puppeteer = require('puppeteer')
+
 const 
   EMAIL_INPUT_SELECTOR = 'input[type="email"]',
   PASSWORD_INPUT_SELECTOR = 'input[type="password"]',
   SUBMINT_INPUT_SELECTOR = 'input[type="submit"]',
+  AMAZON_URL = 'https://www.amazon.com',
   LOGIN_URL = 'https://www.amazon.com/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0'
 
 async function getAmazonSession(){
@@ -10,19 +12,17 @@ async function getAmazonSession(){
   const page = await browser.newPage()
 
   await page.goto(LOGIN_URL)
-  await page.waitForSelector(EMAIL_INPUT_SELECTOR)
+
   let emailInputElement = await page.$(EMAIL_INPUT_SELECTOR)
   await emailInputElement.type(process.env.AMAZON_USERNAME)
-  
+
   let passwordInputElement = await page.$(PASSWORD_INPUT_SELECTOR)
   await passwordInputElement.type(process.env.AMAZON_PASSWORD)
 
   await page.click(SUBMINT_INPUT_SELECTOR)
 
-  const cookies = await page.cookies(LOGIN_URL)
+  const cookies = await page.cookies(AMAZON_URL)
 
   await browser.close()
   return cookies
 }
-
-(async () => console.log(await getAmazonSession()) )

b435173ac7204eca040cc1bab50ec9522ec5e301
Ron Lapushner <lapushnerron@gmail.com>
2017-12-09 23:47:51 -0600

    added participate class

---
+++ giveawayClicker/participate.js
@@ -0,0 +1,8 @@
+Class Participate() {
+  static function noRequirment(){}
+  static function videoRequirment(){}
+  static function tweetRequirment(){}
+  static function followTweeterRequirment(){}
+  static function followAmazonRequirment(){}
+  static function pollRequirment(){}
+}

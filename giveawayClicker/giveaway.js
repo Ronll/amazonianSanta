3d3c7aad31b0b438c7bd89faacdf997d320a46b3
Ron Lapushner <lapushnerron@gmail.com>
2017-12-12 01:57:11 -0600

    added helper functions to be shared by diffrent giveaway requirments

--- giveawayClicker/giveaway.js
+++ giveawayClicker/giveaway.js
@@ -2,15 +2,20 @@ const
   puppeteer = require('puppeteer'),
   request = require('request-promise-native')
 
-const REQ_TYPES = {
-  NONE = 'none',
-  VIDEO = 'watch video for 30 secounds',
-  RETWEET = 'retweet',
-  FOLLOW_TWITTER = 'follow on tweeter',
-  FOLLOW_AMAZON = 'follow on amazon',
-  POLL = 'answer poll questions'
-},
+// const REQ_TYPES = {
+//   NONE = 'none',
+//   VIDEO = 'watch video for 30 secounds',
+//   RETWEET = 'retweet',
+//   FOLLOW_TWITTER = 'follow on tweeter',
+//   FOLLOW_AMAZON = 'follow on amazon',
+//   POLL = 'answer poll questions'
+// },
+const
   AMAZON_GIVEAWAY_URL = 'https://www.amazon.com/ga/p/',
+  SIGN_IN_INPUT_SELECTOR = '#signInSubmit',
+  BOX_DIV_SELECTOR = '#box_click_target',
+  RESULT_SPAN_SELECTOR = '#title',
+  DID_NOT_WIN = 'you didn\'t win'
 
 class Giveaway {
   constructor(id){
@@ -31,14 +36,49 @@ class VideoGiveaway extends Giveaway {
 }
 
 async function openGiveawayPage(id){
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({headless: false})
   const page = await browser.newPage()
 
-  await page.goto(AMAZON_GIVEAWAY_URL + id)
+  await page.goto(AMAZON_GIVEAWAY_URL + id, {waitUntil: 'networkidle0'})
+  
+  if(await isPageLoggedOut(page))
+    throw 'logged out'
 
   return page
 }
 
-async function isPageLoggedIn(page){
-  await page.
+async function isPageLoggedOut(page){
+  let el = await page.$(SIGN_IN_INPUT_SELECTOR)
+  if(el)
+    return true
+  else
+    return false
+}
+
+async function openBoxAndGetResult(page){
+  await openBox(oage)
+  let didWin = await getResult(page)
+  return didWin
+}
+
+async function openBox(page){
+  try{
+    await page.waitForSelector(BOX_DIV_SELECTOR, {visible: true})
+    await page.click(BOX_DIV_SELECTOR)
+  }catch(e){
+    await page.screenshot({path: 'could_not_open_box.png'})
+    throw 'could not open box'
+  }
+}
+
+async function getResult(page){
+  await page.waitForSelector(RESULT_SPAN_SELECTOR, {visible: true})
+  let el = page.$(RESULT_SPAN_SELECTOR)
+  let valueFieldProperty = await el.getProperty('value')
+  let result = await valueFieldProperty.jsonValue()
+
+  if(result.includes(DID_NOT_WIN))
+    return false
+  else
+    return true
 }

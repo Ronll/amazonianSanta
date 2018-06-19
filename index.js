995d6fca41bc46e8d0540b9bf35811fd3c3ac794
Ron Lapushner <lapushnerron@gmail.com>
2018-06-18 20:35:18 -0500

    fixed index.js to work with all components

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const
-  GiveawayStream = require('./giveawayFeed/giveawayStream'),
-  GiveawayFilter = require('./giveawayFeed/giveawayFilter')
+(async () =>{
 
-var giveawayFeed = new GiveawayStream()
-var giveawayFilter = new GiveawayFilter()
+  const
+    GiveawayStream = require('./giveawayFeed/giveawayStream'),
+    GiveawayFilter = require('./giveawayFeed/giveawayFilter'),
+    AmazonParticipator = require('./amazonParticipator/AmazonParticipator')
+  
+  var giveawayFeed = new GiveawayStream()
+  var giveawayFilter = new GiveawayFilter()
+  var giveawayParticipator = await new AmazonParticipator().constructorAsync()
+  
+  giveawayFeed.pipe(giveawayFilter).on('readable', async () => {
+    for(let ga = giveawayFilter.read(); ga !== null; ga = giveawayFilter.read()){
+      console.dir(ga)
+      await giveawayParticipator.participate(ga)
+      console.log(ga.didWin)
+    }
+  })
 
-giveawayFeed.pipe(giveawayFilter).on('readable', () => {
-  console.log('readable')
-  let chunk;
-  while (null !== (chunk = giveawayFilter.read())) {
-    console.log(`Received ${chunk}`);
-  }
-})
-
+})()

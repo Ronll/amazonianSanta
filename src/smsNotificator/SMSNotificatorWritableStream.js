7a48ed66a26588eeffa5538730603b333579a484
Ron Lapushner <lapushnerron@gmail.com>
2018-07-22 01:01:24 -0500

    added a link to the giveaway in the SMS notification

--- src/smsNotificator/SMSNotificatorWritableStream.js
+++ src/smsNotificator/SMSNotificatorWritableStream.js
@@ -16,8 +16,15 @@ class SMSNotificatorWritableStream extends stream.Writable {
 
   _write(ga, encoding, callback) {
     if(ga.didWin === true){
-      this._sms.send(`Won: ${ga.title}, Image:${ga.productImageURL}`)
+      
+      this._sms.send(
+        `Won: ${ga.title}, ` + 
+        `Image:${ga.productImageURL}, ` +
+        `Link: https://www.amazon.com/ga/p/${ga.amazonID}`
+      )
+
       log.info(`Sent winning notification: amazonID: ${ga.amazonID}, Title: ${ga.title}`)
+      
     }else
       log.debug(`Did not send sms for unwon giveaway: amazonID: ${ga.amazonID}`)
 

b12334322d507dc6c135d83d395a25627bd1678d
Ron Lapushner <lapushnerron@gmail.com>
2018-11-29 19:54:17 -0800

    now using rate limiter

--- index.js
+++ index.js
@@ -3,17 +3,20 @@
   const
     GiveawayStream = require('./src/giveawayFeed/GiveawayStream'),
     GiveawayFilter = require('./src/giveawayFilter/GiveawayFilter'),
+    GiveawayRateLimiter = require('./src/giveawayRateLimiter/GiveawayRateLimiter'),
     AmazonParticipatorTransform = require('./src/amazonParticipator/AmazonParticipatorTransform'),
     SMSNotificatorWritableStream = require('./src/smsNotificator/SMSNotificatorWritableStream')
   
   let
     giveawayFeed = new GiveawayStream(),
     giveawayFilter = new GiveawayFilter(),
+    giveawayRateLimiter = new GiveawayRateLimiter(),
     amazonParticipatorTransform = await new AmazonParticipatorTransform().constructorAsync(),
     smsNotificatorWritableStream = new SMSNotificatorWritableStream()
   
   giveawayFeed
     .pipe(giveawayFilter)
+    .pipe(giveawayRateLimiter)
     .pipe(amazonParticipatorTransform)
     .pipe(smsNotificatorWritableStream)
 
